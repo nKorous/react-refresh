dea7d26dc3e06e0575f70e492a7a22979ebe51ae
nKorous <nathaniel.korous@gmail.com>
2020-12-23 10:36:14 -0700

    topbar login basic compelte

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import { Button } from '@material-ui/core'
+
 
 function App() {
+  const [ list, setList ] = useState<Array<any>>()
+  const [ user, setUser ] = useState<string | null>(null)
+  const [ loggedIn, setUserLoggedIn ] = useState<boolean>(false)
+
+  useEffect(() => {
+    setList([])
+  },[])
+
+  const login = () => {
+    setUserLoggedIn(true)
+  }
+
+  const logout = () => {
+    setUserLoggedIn(false)
+    setUser(null)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='topbar'>
+        { !loggedIn && <span className='loginSpan'><input type='username' onChange={(event) => {setUser(event.target.value)}} placeholder='Name' style={{marginLeft: '5px'}}/> <Button onClick={() => login()}>Login</Button> </span>}
+        { loggedIn && <span className='loginSpan'><Button onClick={() => logout()}>Logout</Button></span> }
+
+        <span style={{ flex: '1 1'}}></span>
+
+        { !loggedIn && <span className='welcomeSpan'>Welcome! </span> }
+        { loggedIn && <span className='welcomeSpan'>Welcome, { user }!</span> }
+
+        { loggedIn &&
+          <div>
+
+          </div>   
+        }
+      </div>
     </div>
   );
 }
